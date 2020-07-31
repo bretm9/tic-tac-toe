9620c3e63fb09bbe640a9cf69b053b63a610416a
Bret Merritt <merrittbret9@gmail.com>
2020-07-31 16:16:31 -0600

    Add player properties in constructor and checkForWin logic

--- src/game.js
+++ src/game.js
@@ -1,11 +1,16 @@
 class Game {
-    constructor(board, turn) {
-        this.board = ;
+    constructor(board, turn, player1, player2) {
+        this.board = [];
         this.turn = turn || 1;
+        this.player1 = player1 || new Player();
+        this.player2 = player2 || new Player();
     }
 
-    checkForWins() {
-
+    checkForWin() {
+        if (this.board.includes(winCombos)) {
+            return player.turn;
+        }
+        return "Draw!";
     }
 
     saveBoardToPlayersWins() {
@@ -17,7 +22,7 @@ class Game {
     }
 
     saveBoardToStorage() {
-        
+
     }
 
 }
