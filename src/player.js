ae169da986ad62f88a72c7f95fd08f926f4e3501
Bret Merritt <merrittbret9@gmail.com>
2020-07-31 15:55:09 -0600

    Add localStorage save and retrieve functions, set wins property to empty array in constructor

--- src/player.js
+++ src/player.js
@@ -2,6 +2,15 @@ class Player {
     constructor(id, token, wins) {
         this.id = id;
         this.token = token;
-        this.wins = wins || 0;
+        this.wins = wins || [];
     }
+
+    saveWinsFromStorage() {
+        localStorage.setItem(`player-${id}-wins`, JSON.stringify(this.wins));
+    }
+
+    retrieveWinsFromStorage() {
+        localStorage.getItem(`player-${id}-wins`);
+    }
+
 }
