17b0080de8aaf4ee7d44e16486a62970cfe78e8d
Bret Merritt <merrittbret9@gmail.com>
2020-08-01 14:39:20 -0600

    Revert Game.board to simple array, add updateGameState functionality

--- src/game.js
+++ src/game.js
@@ -1,31 +1,39 @@
 class Game {
-    constructor(board, turn, player1, player2) {
-        this.board = [
-            [0,0,0],
-            [0,0,0],
-            [0,0,0]
-        ];
+    constructor(board, turn, player1, player2, gameState) {
+        this.board = [0,0,0,0,0,0,0,0,0];
         this.turn = turn || 1;
         this.player1 = player1 || new Player(1,1);
         this.player2 = player2 || new Player(2,2);
+        this.gameState = "turn";
         this.winningCombos = [
-            [1,2,3],
-            [4,5,6],
-            [7,8,9],
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
             [1,4,7],
             [2,5,8],
-            [3,6,9],
-            [1,5,9],
-            [3,5,7]
+            [0,4,8],
+            [2,4,6]
         ];
     }
 
-    checkPlayerTurn() {
+    checkCurrentPlayer() {
         if (this.turn === 1) {
             return this.player1;
         } else {
             return this.player2;
         }
+    }
+
+    updateGameState() {
+        if (this.checkForWin()) {
+            this.gameState = "wins!";
+        }
+        if (this.checkForDraw()) {
+            this.gameState = "Draw!"
+        }
+        this.changeTurn();
+        return this.gameState;
     }
 
     changeTurn() {
@@ -36,12 +44,14 @@ class Game {
         }
     }
 
+    
+
     checkForWin() {
         var winComboMoves;
         for (var i = 0; i < this.winningCombos.length; i++) {
             winComboMoves = 0;
             for (var j = 0; j < 3; j++) {
-                if (this.checkPlayerTurn().moves.includes(this.winningCombos[i][j])) {
+                if (this.checkCurrentPlayer().moves.includes(this.winningCombos[i][j])) {
                      winComboMoves++;
                 }
                 if (winComboMoves === 3) {
@@ -54,8 +64,8 @@ class Game {
 
     checkForDraw() {
         for (var i = 0; i < this.board.length; i++) {
-            if (!this.board[i].includes(0)) {
-                return !this.checkForWin() ;
+            if (this.board[i] != 0) {
+                return !this.checkForWin();
             }
         }
         return false;
