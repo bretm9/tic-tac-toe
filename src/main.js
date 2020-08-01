d5c14723e1bf38c2c0198580023ba83f139eae01
Bret Merritt <merrittbret9@gmail.com>
2020-08-01 13:06:11 -0600

    Add querySelectors

---
+++ src/main.js
@@ -0,0 +1,6 @@
+var players = document.querySelectorAll(".section-player");
+var board = document.querySelector(".board");
+var wins = document.querySelector(".wins");
+// var gameOverOverlay = document.querySelector(".game-over-overlay");
+
+var newGame = new Game();
